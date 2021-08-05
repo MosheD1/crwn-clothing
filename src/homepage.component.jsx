3fd8d60b8da704a829181d7a699800357a759395
Moshe Dego <moshedego@gmail.com>
2021-08-05 13:17:55 +0300

    add file for homepage

---
+++ src/homepage.component.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import './homepage.style.scss'
+
+const HomePage = () => {
+    return (
+        <div className='homepage'>
+            <div className='directory-menue'>
+                <div className='menu-item'>
+                    <div className='content'>
+                        <h1 className='title'>Hats</h1>
+                        <span className='subtitle'>Shop</span>
+                    </div>
+                </div>
+                <div className='menu-item'>
+                    <div className='content'>
+                        <h1 className='title'>Hats</h1>
+                        <span className='subtitle'>Shop</span>
+                    </div>
+                </div>
+                <div className='menu-item'>
+                    <div className='content'>
+                        <h1 className='title'>Hats</h1>
+                        <span className='subtitle'>Shop</span>
+                    </div>
+                </div>
+                <div className='menu-item'>
+                    <div className='content'>
+                        <h1 className='title'>Hats</h1>
+                        <span className='subtitle'>Shop</span>
+                    </div>
+                </div>
+                <div className='menu-item'>
+                    <div className='content'>
+                        <h1 className='title'>Hats</h1>
+                        <span className='subtitle'>Shop</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default HomePage;
